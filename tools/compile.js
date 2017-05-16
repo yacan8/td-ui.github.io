89894eedc39bf48ccbae319db594381e74d25bda
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2017-05-16 16:13:14 +0800

    调整代码结构

--- tools/compile.js
+++ tools/compile.js
@@ -5,19 +5,8 @@
  * @Last modified time: 2017-05-15 16:08:34
  */
 
-import logger from './utils/logger';
-import gulp from 'gulp';
+import runGulpTask from './runGulpTask';
 
 export default async function compile() {
-  require('./gulpfile');
-  await new Promise((resolve, reject) => {
-    gulp.start('compile', function(err) {
-      if (err) {
-        logger.error(err);
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+await runGulpTask('compile');
 }
--- tools/component.js
+++ tools/component.js
@@ -5,19 +5,8 @@
  * @Last modified time: 2017-05-16 15:31:08
  */
 
-import logger from './utils/logger';
-import gulp from 'gulp';
+import runGulpTask from './runGulpTask';
 
-export default async function compile() {
- require('./gulpfile');
- await new Promise((resolve, reject) => {
-   gulp.start('component', function(err) {
-     if (err) {
-       logger.error(err);
-       reject(err);
-     } else {
-       resolve();
-     }
-   });
- });
+export default async function component() {
+ await runGulpTask('component');
 }
