bf35dec10bbb642d1ff2544006833064bbf9c34e
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2017-05-16 11:48:09 +0800

    add multi compile

--- tools/compile.js
+++ tools/compile.js
@@ -5,41 +5,19 @@
  * @Last modified time: 2017-05-15 16:08:34
  */
 
-import getTDConfig from '../tdtool.config.comp'
-import logger from './utils/logger'
-import shell from 'shelljs'
-import webpack from 'webpack'
-
-function build(config) {
-  return new Promise((resolve, reject) => {
-    webpack(config).run((err, stats) => {
-      if (err) {
-        logger.error(err);
-        return reject(err);
-      }
-
-      logger.info(stats.toString({
-        colors: true,
-        timings: true,
-      }));
-      return resolve();
-    })
-  })
-}
+import logger from './utils/logger';
+import gulp from 'gulp';
 
 export default async function compile() {
-  const argv = require('yargs').parse(process.argv.slice(3));
-  const allComponents = shell.ls('./components')
-  const components = argv._
-  if (components.length) {
-    for (let i = 0; i < components.length; i++) {
-      // await build(getTDConfig(components[i]))
-      await shell.exec(`babel components/${components[i]} --out-dir lib/${components[i]}`)
-    }
-  } else {
-    for (let i = 0; i < allComponents.length; i++) {
-      // await build(getTDConfig(allComponents[i]))
-      await shell.exec(`babel components/${allComponents[i]} --out-dir lib/${allComponents[i]}`)
-    }
-  }
+  require('./gulpfile');
+  await new Promise((resolve, reject) => {
+    gulp.start('compile', function(err) {
+      if (err) {
+        logger.error(err);
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -4,3 +4,102 @@
  * @Last modified by:   yzf
  * @Last modified time: 2017-05-15 18:54:44
  */
+
+import { execSync } from 'child_process';
+import gulpLogger from 'gulp-logger';
+import through2 from 'through2';
+import babel from 'gulp-babel';
+import shell from 'shelljs';
+import merge2 from 'merge2';
+import gulp from 'gulp';
+
+import transformLess from './utils/transformLess';
+import logger from './utils/logger';
+
+function babelify(js, dest) {
+  return js
+    .pipe(gulpLogger({
+      beforeEach: 'Begin to compile ',
+      colors: true
+    }))
+    .pipe(babel({
+      presets: [
+        'es2015-ie',
+        'react',
+        'stage-2',
+      ],
+      plugins: [
+        'transform-decorators-legacy',
+        'transform-class-properties',
+        'transform-runtime'
+      ]
+    }))
+    .pipe(through2.obj(function(file, encoding, next) {
+      this.push(file.clone());
+      if (file.path.match(/\/style\/index(\.web)?\.js/)) {
+        const content = file.contents.toString(encoding);
+        if (file.path.indexOf('.web.js') === -1 && content.indexOf('.less\'') === -1) {
+          next();
+          return;
+        }
+        file.contents = new Buffer(content
+          .replace(/\/style\/?'/g, '/style/css\'')
+          .replace(/\.less/g, '.css'));
+        file.path = file.path.replace(/index(\.web)?\.js/, 'css$1.js');
+        this.push(file);
+        next();
+      } else {
+        next();
+      }
+    }))
+    .pipe(gulp.dest(dest));
+}
+
+function compile(dir, dest) {
+  const less = gulp.src([`${dir}/**/*.less`])
+    .pipe(gulpLogger({
+      beforeEach: 'Begin to compile ',
+      colors: true
+    }))
+    .pipe(through2.obj(function(file, encoding, next) {
+      this.push(file.clone());
+      if (file.path.match(/\/style\/index(\.web)?\.less$/)) {
+        transformLess(file.path).then(css => {
+          file.contents = new Buffer(css);
+          file.path = file.path.replace(/\.less$/, '.css');
+          this.push(file);
+          next();
+        }).catch(e => {
+          logger.error(e);
+        });
+      } else {
+        next();
+      }
+    }))
+    .pipe(gulp.dest(dest));
+  const assets = gulp.src([`${dir}/**/*.@(png|svg)`])
+    .pipe(gulpLogger({
+      beforeEach: 'Begin to compile ',
+      colors: true
+    }))
+    .pipe(gulp.dest(dest));
+  const js = babelify(gulp.src(`${dir}/**/*.js`), dest);
+  return [less, js, assets];
+}
+
+gulp.task('compile', () => {
+  const argv = require('yargs').parse(process.argv.slice(3));
+  const allComponents = shell.ls('./components');
+  const components = argv._;
+  if (components.length) {
+    let streams = [];
+    components.forEach(item => {
+      execSync(`rimraf lib/${item}`);
+      streams = streams.concat(compile(`components/${item}`, `lib/${item}`));
+    });
+    return merge2(streams);
+  } else {
+    execSync('rimraf lib');
+    return merge2(compile('components', 'lib'));
+  }
+})
