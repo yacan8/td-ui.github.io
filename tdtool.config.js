e68bb570435ba2708a95e6f1448432622f1be902
hihl <348267823@qq.com>
2017-05-23 10:49:24 +0800

    add build script

--- tdtool.config.js
+++ tdtool.config.js
@@ -1,19 +1,25 @@
 process.env.NODE_ENV = 'production'
-const Config = require('tdtool').Config
+
+const path = require('path');
+const Config = require('tdtool').Config;
 const config1 = new Config({
   entry: './components/index',
   filename: 'td-ui.min.js',
   minimize: true,
-  extends: ['react', ['less', {
+  extends: [['react', {
+    source: [path.resolve(process.cwd(), 'components')]
+  }], ['less', {
     extractCss: 'td-ui.min.css'
   }]]
-})
+});
 const config2 = new Config({
   entry: './components/index',
   filename: 'td-ui.js',
-  extends: ['react', ['less', {
+  extends: [['react', {
+    source: [path.resolve(process.cwd(), 'components')]
+  }], ['less', {
     extractCss: 'td-ui.css'
   }]]
-})
+});
 
-module.exports = [config1.resolve(), config2.resolve()]
+module.exports = [config1.resolve(), config2.resolve()];
