460f2188047f4546e4984ab058930fd97992ed3d
can.yang <can.yang@canyangdeMacBook-Air.local>
2017-05-26 17:16:35 +0800

    component demo modify

--- tools/component.js
+++ tools/component.js
@@ -7,8 +7,11 @@
 
 import runGulpTask from './runGulpTask';
 import open from 'open';
+import run from 'tdtool/lib/util/run-command';
+import start from 'tdtool/lib/cmds/start';
 
 export default async function component() {
-  await runGulpTask('component');
+  run(start.bind(undefined,{config:'./tools/tdtool.config.comp.js',port:8080}));
+  // await runGulpTask('component');
   await open('http://localhost:8080');
 }
--- tools/tdtool.config.comp.js
+++ tools/tdtool.config.comp.js
@@ -4,14 +4,20 @@
  * @Last modified by:   yzf
  * @Last modified time: 2017-05-16 17:57:43
  */
-
+import fs from 'fs'
 import path from 'path';
-
-module.exports = component => {
+import logger from './utils/logger';
+module.exports =((component) => {
   const Config = require('tdtool').Config;
+  const entry = `components/${component}/demo/index.js`;
+  if (!fs.existsSync(path.resolve(process.cwd(), entry))) {
+    logger.fatal(`Demo file ${entry} does not exist.`)
+    return
+  }
   const config = new Config({
-    entry: `./components/${component}/demo/index`,
+    entry: `./${entry}`,
     dist: './dist/' + component,
+    devServer:true,
     sourceMap: true,
     extends: [['react', {
       eslint: true,
@@ -21,5 +27,5 @@ module.exports = component => {
     }]],
     template: './tools/index.tpl'
   });
-  return config.resolve();
-}
+  return [config.resolve()];
+})(require('yargs').parse(process.argv.slice(3))._[0])
